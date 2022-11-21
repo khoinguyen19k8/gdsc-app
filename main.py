361fb04a3693e349cee3839fc42cb6fd7568d017
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2022-11-21 19:44:12 +0100

    Add multithreading template code

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
+from queue import Queue
+import threading
 import time
-import sounddevice as sd
-import speech_recognition as sr
 
 
-# this is called from the background thread
-def callback(recognizer, audio):
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print(
-            "Whisper thinks you said "
-            + recognizer.recognize_whisper(audio, language="english")
-        )
-    except sr.UnknownValueError:
-        print("Whisper could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Whisper")
+class RecordingService(threading.Thread):
+    def __init__(self, audio_segments: Queue):
+        threading.Thread.__init__(self)
+        self.audio_segments = audio_segments
+
+    def run(self):
+        while True:
+            self.audio_segments.put(time.time())
+            time.sleep(1)
 
 
-r = sr.Recognizer()
-m = sr.Microphone()
+class TranscriptionService(threading.Thread):
+    def __init__(self, audio_segments: Queue):
+        threading.Thread.__init__(self)
+        self.audio_segments = audio_segments
 
-with m as source:
-    r.adjust_for_ambient_noise(
-        source
-    )  # we only need to calibrate once, before we start listening
+    def run(self):
+        while True:
+            result = self.audio_segments.get()
+            print(result)
+            self.audio_segments.task_done()
 
-stop_listening = r.listen_in_background(m, callback, phrase_time_limit=1)
 
-print("Hello world")
+audio_segments = Queue()
+recorder = RecordingService(audio_segments)
+transcriber = TranscriptionService(audio_segments)
 
-while True:
-    time.sleep(1)
-
-stop_listening(wait_for_stop=False)
+recorder.start()
+transcriber.start()
