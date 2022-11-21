01c22c26235381d622dc11c6ef0ec4d3a073a7f3
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2022-11-21 21:28:22 +0100

    move the transcription part to TranscriptionService

--- main.py
+++ main.py
@@ -3,45 +3,48 @@ import threading
 import time
 import speech_recognition as sr
 import sounddevice as sd
+from pydub import AudioSegment
+import scipy
+from whisper import audio
+import numpy as np
 
 
 class RecordingService(threading.Thread):
-    def __init__(self, audio_segments: Queue):
+    def __init__(self, audio_segments: Queue, sample_rate=44100):
         threading.Thread.__init__(self)
         self.r = sr.Recognizer()
-        self.m = sr.Microphone()
+        self.m = sr.Microphone(sample_rate=sample_rate)
         self.audio_segments = audio_segments
         with self.m as source:
             self.r.adjust_for_ambient_noise(source)
 
     def run(self):
         with self.m as source:
+            print("Started recording!")
             while True:
-                audio = self.r.listen(source)
-                self.audio_segments.put(
-                    self.r.recognize_whisper(audio, language="english")
-                )
-                time.sleep(0.5)
+                audio = self.r.listen(source, phrase_time_limit=10)
+                self.audio_segments.put(audio)
 
 
 class TranscriptionService(threading.Thread):
     def __init__(self, audio_segments: Queue):
         threading.Thread.__init__(self)
         self.audio_segments = audio_segments
+        self.r = sr.Recognizer()
+        self.m = sr.Microphone()
 
     def run(self):
-        with open("transcript.txt", "a+") as f:
-            while True:
-                result = self.audio_segments.get()
-                print(f'Person: "{result}"')
-                f.write(f"{result}\n")
-                self.audio_segments.task_done()
+        while True:
+            audio_result = self.audio_segments.get()
+            transcription = self.r.recognize_whisper(audio_result, language="english")
+            print(f'Person: "{transcription}"')
+            self.audio_segments.task_done()
 
 
-audio_segments = Queue()
-recorder = RecordingService(audio_segments)
-transcriber = TranscriptionService(audio_segments)
+if __name__ == "__main__":
+    audio_segments = Queue()
+    recorder = RecordingService(audio_segments)
+    transcriber = TranscriptionService(audio_segments)
 
-recorder.start()
-transcriber.start()
-transcriber.join()
+    recorder.start()
+    transcriber.start()
