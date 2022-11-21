db1f71fe90b3ebde96cbad3512fe051d5447c691
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2022-11-21 20:48:44 +0100

    first working multi-threading version that can listen and transcribe on the fly

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from queue import Queue
 import threading
 import time
+import speech_recognition as sr
+import sounddevice as sd
 
 
 class RecordingService(threading.Thread):
     def __init__(self, audio_segments: Queue):
         threading.Thread.__init__(self)
+        self.r = sr.Recognizer()
+        self.m = sr.Microphone()
         self.audio_segments = audio_segments
+        with self.m as source:
+            self.r.adjust_for_ambient_noise(source)
 
     def run(self):
-        while True:
-            self.audio_segments.put(time.time())
-            time.sleep(1)
+        with self.m as source:
+            while True:
+                audio = self.r.listen(source)
+                self.audio_segments.put(
+                    self.r.recognize_whisper(audio, language="english")
+                )
+                time.sleep(0.5)
 
 
 class TranscriptionService(threading.Thread):
@@ -20,10 +30,12 @@ class TranscriptionService(threading.Thread):
         self.audio_segments = audio_segments
 
     def run(self):
-        while True:
-            result = self.audio_segments.get()
-            print(result)
-            self.audio_segments.task_done()
+        with open("transcript.txt", "a+") as f:
+            while True:
+                result = self.audio_segments.get()
+                print(f'Person: "{result}"')
+                f.write(f"{result}\n")
+                self.audio_segments.task_done()
 
 
 audio_segments = Queue()
@@ -32,3 +44,4 @@ transcriber = TranscriptionService(audio_segments)
 
 recorder.start()
 transcriber.start()
+transcriber.join()
